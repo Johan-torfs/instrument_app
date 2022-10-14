b42dc73558ebfc3b5bdd8e304c6b5f444dfba5ea
Johan Torfs <r0831282@student.thomasmore.be>
2022-10-14 17:09:28 +0200

    init

---
+++ lib/widgets/armultipletargets.dart
@@ -0,0 +1,83 @@
+import 'package:augmented_reality_plugin_wikitude/architect_widget.dart';
+import 'package:augmented_reality_plugin_wikitude/startupConfiguration.dart';
+import 'package:flutter/material.dart';
+import '../env/keys.dart';
+
+class ArMultipleTargetsWidget extends StatefulWidget {
+  const ArMultipleTargetsWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() =>
+      _ArMultipleTargetsWidgetState();
+}
+
+class _ArMultipleTargetsWidgetState extends State<ArMultipleTargetsWidget>
+    with WidgetsBindingObserver {
+  late ArchitectWidget architectWidget;
+  String wikitudeTrialLicenseKey = WIKITUDE_KEY;
+  StartupConfiguration startupConfiguration = StartupConfiguration(
+      cameraPosition: CameraPosition.BACK,
+      cameraResolution: CameraResolution.AUTO);
+  List<String> features = ["image_tracking"];
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+
+    architectWidget = ArchitectWidget(
+      onArchitectWidgetCreated: onArchitectWidgetCreated,
+      licenseKey: wikitudeTrialLicenseKey,
+      startupConfiguration: startupConfiguration,
+      features: features,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.black),
+      child: architectWidget, //ar widget
+    );
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.paused:
+        architectWidget.pause();
+        break;
+      case AppLifecycleState.resumed:
+        architectWidget.resume();
+        break;
+
+      default:
+    }
+  }
+
+  @override
+  void dispose() {
+    architectWidget.pause();
+    architectWidget.destroy();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<void> onArchitectWidgetCreated() async {
+    architectWidget.load(
+        "samples/03_MultipleTargets_1_MultipleTargets/index.html",
+        onLoadSuccess,
+        onLoadFailed);
+    architectWidget.resume();
+  }
+
+  Future<void> onLoadSuccess() async {
+    debugPrint("Successfully loaded Architect World");
+  }
+
+  Future<void> onLoadFailed(String error) async {
+    debugPrint("Failed to load Architect World");
+    debugPrint(error);
+  }
+}
