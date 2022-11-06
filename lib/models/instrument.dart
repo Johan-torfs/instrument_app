6f08dd16ffa629c6ca78a118075f8df9e58d818b
JohanTorfs <r0831282@student.thomasmore.be>
2022-11-06 16:35:30 +0100

    fix capitalcase instrument filename

---
+++ lib/models/instrument.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import './piece.dart';
+import './musician.dart';
+
+class Instrument {
+  final int id;
+  final String name;
+  final String period;
+  final String description;
+  final String collection;
+
+  const Instrument({
+    required this.id,
+    required this.name,
+    required this.period,
+    required this.description,
+    required this.collection,
+  });
+
+  factory Instrument.fromJson(Map<String, dynamic> json) {
+    return Instrument(
+      id: int.parse(json['id']),
+      name: utf8.decode(latin1.encode(json['name'])),
+      period: utf8.decode(latin1.encode(json['period'])),
+      description: utf8.decode(latin1.encode(json['description'])),
+      collection: utf8.decode(latin1.encode(json['collection'])),
+    );
+  }
+}
