8edbbaddee2756c3c6f63d6919a14bd1d0c08674
JohanTorfs <r0831282@student.thomasmore.be>
2022-11-06 15:01:26 +0100

    add muscian model

---
+++ lib/models/musician.dart
@@ -0,0 +1,25 @@
+class Musician {
+  final int id;
+  final String name;
+  final int yearOfBirth;
+  final int yearOfDeath;
+  final Instrument instrument;
+
+  const Musician({
+    required this.id,
+    required this.name,
+    required this.yearOfBirth,
+    required this.yearOfDeath,
+    required this.instrument,
+  });
+
+  factory Musician.fromJson(Map<String, dynamic> json) {
+    return Musician(
+      id: int.parse(json['id']),
+      name: json['name'],
+      yearOfBirth: json['yearOfBirth'],
+      yearOfDeath: json['yearOfDeath'],
+      instrument: Instrument.fromJson(json['instrument']),
+    );
+  }
+}
