fd787863610ce250a2111c94e86cbbd1c4defe04
Ebert <ebert@r-vo.net>
2022-11-10 20:00:34 +0100

    object detection
    Changed the wikitude mode to detecting objects instead of pictures
    Generated and added a wto for a violin, which hasn't yet been proven to work

--- samples/03_MultipleTargets_1_MultipleTargets/js/multiplesimultaneoustargets.js
+++ samples/03_MultipleTargets_1_MultipleTargets/js/multiplesimultaneoustargets.js
@@ -1,93 +1,113 @@
 var World = {
     loaded: false,
-    dinoSettings: {
-        diplodocus: {
-            scale: 0.4
-        },
-        spinosaurus: {
-            scale: 0.004
-        },
-        triceratops: {
-            scale: 0.4
-        },
-        tyrannosaurus: {
-            scale: 0.4
-        }
+    drawables: [],
+    firetruckRotation: {
+        x: 0,
+        y: 0,
+        z: 0
     },
+    firetruckCenter: {
+        x: 0,
+        y: -0.14,
+        z: 0
+    },
+    firetruckLength: 0.33,
+    firetruckHeight: 0.2,
 
     init: function initFn() {
-        this.createOverlays();
+        World.createOccluder();
+        World.createCones();
+        World.createTracker();
     },
 
-    createOverlays: function createOverlaysFn() {
-        /*
-            First a AR.TargetCollectionResource is created with the path to the Wikitude Target Collection(.wtc) file.
-            This .wtc file can be created from images using the Wikitude Studio. More information on how to create them
-            can be found in the documentation in the TargetManagement section.
-            Each target in the target collection is identified by its target name. By using this
-            target name, it is possible to create an AR.ImageTrackable for every target in the target collection.
-         */
-        this.targetCollectionResource = new AR.TargetCollectionResource("assets/dinosaurs.wtc", {
-            onError: World.onError
-        });
+    createOccluder: function createOccluderFn() {
+        var occluderScale = 0.0065 * this.firetruckLength;
 
-        /*
-            This resource is then used as parameter to create an AR.ImageTracker. Optional parameters are passed as
-            object in the last argument. In this case a callback function for the onTargetsLoaded trigger is set. Once
-            the tracker loaded all of its target images this callback function is invoked. We also set the callback
-            function for the onError trigger which provides a sting containing a description of the error.
-
-            To enable simultaneous tracking of multiple targets 'maximumNumberOfConcurrentlyTrackableTargets' has
-            to be set.
-            to be set.
-         */
-        this.tracker = new AR.ImageTracker(this.targetCollectionResource, {
-            maximumNumberOfConcurrentlyTrackableTargets: 5, // a maximum of 5 targets can be tracked simultaneously
-            /*
-                Disables extended range recognition.
-                The reason for this is that extended range recognition requires more processing power and with multiple
-                targets the SDK is trying to recognize targets until the maximumNumberOfConcurrentlyTrackableTargets
-                is reached and it may slow down the tracking of already recognized targets.
-             */
-            extendedRangeRecognition: AR.CONST.IMAGE_RECOGNITION_RANGE_EXTENSION.OFF,
-            onTargetsLoaded: World.showInfoBar,
-            onError: World.onError
-        });
-
-        /*
-            Pre-load models such that they are available in cache to avoid any slowdown upon first recognition.
-         */
-        new AR.Model("assets/models/diplodocus.wt3");
-        new AR.Model("assets/models/spinosaurus.wt3");
-        new AR.Model("assets/models/triceratops.wt3");
-        new AR.Model("assets/models/tyrannosaurus.wt3");
-
-        /*
-            Note that this time we use "*" as target name. That means that the AR.ImageTrackable will respond to
-            any target that is defined in the target collection. You can use wildcards to specify more complex name
-            matchings. E.g. 'target_?' to reference 'target_1' through 'target_9' or 'target*' for any targets
-            names that start with 'target'.
-         */
-        this.dinoTrackable = new AR.ImageTrackable(this.tracker, "*", {
-            onImageRecognized: function(target) {
-                /*
-                    Create 3D model based on which target was recognized.
-                 */
-                var model = new AR.Model("assets/models/" + target.name + ".wt3", {
-                    scale: World.dinoSettings[target.name].scale,
-                    rotate: {
-                        z: 180
-                    },
-                    onError: World.onError
-                });
-
-                /* Adds the model as augmentation for the currently recognized target. */
-                this.addImageTargetCamDrawables(target, model);
-
-                World.hideInfoBar();
+        this.firetruckOccluder = new AR.Occluder("assets/firetruck_occluder.wt3", {
+            onLoaded: World.showInfoBar,
+            scale: {
+                x: occluderScale,
+                y: occluderScale,
+                z: occluderScale
+            },
+            translate: this.firetruckCenter,
+            rotate: {
+                x: 180
             },
             onError: World.onError
         });
+        World.drawables.push(this.firetruckOccluder);
+    },
+
+    createCones: function createConesFn() {
+        var coneDistance = this.firetruckLength * 0.8;
+
+        var frontLeftCone = World.getCone(-coneDistance, +coneDistance);
+        World.drawables.push(frontLeftCone);
+
+        var backLeftCone = World.getCone(+coneDistance, +coneDistance);
+        World.drawables.push(backLeftCone);
+
+        var backRightCone = World.getCone(+coneDistance, -coneDistance);
+        World.drawables.push(backRightCone);
+
+        var frontRightCone = World.getCone(-coneDistance, -coneDistance);
+        World.drawables.push(frontRightCone);
+    },
+
+    getCone: function getConeFn(positionX, positionZ) {
+        var coneScale = 0.05 * this.firetruckLength;
+
+        return new AR.Model("assets/traffic_cone.wt3", {
+            scale: {
+                x: coneScale,
+                y: coneScale,
+                z: coneScale
+            },
+            translate: {
+                x: positionX,
+                y: World.firetruckCenter.y,
+                z: positionZ
+            },
+            rotate: {
+                x: -90
+            },
+            onError: World.onError
+        });
+    },
+
+    createTracker: function createTrackerFn() {
+        this.targetCollectionResource = new AR.TargetCollectionResource("assets/firetruck.wto", {
+            onError: World.onError
+        });
+
+        this.tracker = new AR.ObjectTracker(this.targetCollectionResource, {
+            onError: World.onError
+        });
+
+        this.objectTrackable = new AR.ObjectTrackable(this.tracker, "*", {
+            drawables: {
+                cam: World.drawables
+            },
+            onObjectRecognized: World.objectRecognized,
+            onObjectLost: World.objectLost,
+            onError: World.onError
+        });
+    },
+
+    objectRecognized: function objectRecognizedFn() {
+        World.hideInfoBar();
+        World.setAugmentationsEnabled(true);
+    },
+
+    objectLost: function objectLostFn() {
+        World.setAugmentationsEnabled(false);
+    },
+
+    setAugmentationsEnabled: function setAugmentationsEnabledFn(enabled) {
+        for (var i = 0; i < World.drawables.length; i++) {
+            World.drawables[i].enabled = enabled;
+        }
     },
 
     onError: function onErrorFn(error) {
